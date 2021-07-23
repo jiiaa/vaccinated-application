436e50f17a198bc65a5fbfccff37eacfd901b096
jiiaa <jukkaa@live.com>
2021-07-23 17:16:52 +0300

    Query categories added to the form.

--- frontend/src/components/ViewTotal.js
+++ frontend/src/components/ViewTotal.js
@@ -2,12 +2,17 @@ import React, { useState } from 'react';
 import dbService from '../services/dbService';
 
 const ViewTotal = () => {
+  const [searchCat, setSearchCat] = useState('orderstotal');
   const [searchRange, setSearchRange] = useState('dateonly');
   const [searchDate, setSearchDate] = useState('');
   const [searchTime, setSearchTime] = useState('');
   const [totals, setTotals] = useState({});
 
-  const handleRadioChange = (e) => {
+  const handleCatChange = (e) => {
+    setSearchCat(e.target.value);
+  };
+
+  const handleTimeChange = (e) => {
     setSearchRange(e.target.value);
   };
 
@@ -30,13 +35,79 @@ const ViewTotal = () => {
         <div>
           <input
             type="radio"
+            id="orderstotal"
+            name="category"
+            value="orderstotal"
+            checked={searchCat === 'orderstotal'}
+            onChange={handleCatChange}
+          />
+          <label htmlFor="orderstotal">Orders/vaccines arrived</label>
+          <input
+            type="radio"
+            id="vaccinated"
+            name="category"
+            value="vaccinated"
+            checked={searchCat === 'vaccinated'}
+            onChange={handleCatChange}
+          />
+          <label htmlFor="vaccinated">Vaccinations given</label>
+          <input
+            type="radio"
+            id="perproducer"
+            name="category"
+            value="perproducer"
+            checked={searchCat === 'perproducer'}
+            onChange={handleCatChange}
+          />
+          <label htmlFor="perproducer">Orders/vaccines per producer</label>
+          <input
+            type="radio"
+            id="expired"
+            name="category"
+            value="expired"
+            checked={searchCat === 'expired'}
+            onChange={handleCatChange}
+          />
+          <label htmlFor="category">Bottles expired</label>
+          <input
+            type="radio"
+            id="wasted"
+            name="category"
+            value="wasted"
+            checked={searchCat === 'wasted'}
+            onChange={handleCatChange}
+          />
+          <label htmlFor="wasted">Wasted vaccines</label>
+          <input
+            type="radio"
+            id="lefttouse"
+            name="category"
+            value="lefttouse"
+            checked={searchCat === 'lefttouse'}
+            onChange={handleCatChange}
+          />
+          <label htmlFor="lefttouse">Vaccines left</label>
+          <input
+            type="radio"
+            id="toexpire"
+            name="category"
+            value="toexpire"
+            checked={searchCat === 'toexpire'}
+            onChange={handleCatChange}
+          />
+          <label htmlFor="toexpire">To expire in 10 days</label>
+        </div>
+
+        <div>
+          <input
+            type="radio"
             id="dateonly"
             name="dayorrange"
             value="dateonly"
             checked={searchRange === 'dateonly'}
-            onChange={handleRadioChange}
+            onChange={handleTimeChange}
           />
-          <label htmlFor="dayonly">This date only</label>
+          <label htmlFor="dateonly">This date only</label>
         </div>
         <div>
           <input
@@ -45,10 +116,11 @@ const ViewTotal = () => {
             name="dayorrange"
             value="range"
             checked={searchRange === 'range'}
-            onChange={handleRadioChange}
+            onChange={handleTimeChange}
           />
           <label htmlFor="range">Until the date (including date)</label>
         </div>
+
         <div>
           <input
             type="date"
